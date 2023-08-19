9fef585975ba4877b5d81c33da28ee7e96a82d43
Monali Valunj <monavalunj96@gmail.com>
2023-08-18 22:52:48 -0700

    Math operations solved

--- js/math.js
+++ js/math.js
@@ -1,9 +1,21 @@
 //STEP 1
+// var num = window.prompt("Enter a negative number: ");
+// console.log(Math.abs(num));
 
 //STEP 2
+// var num = window.prompt("Enter a decimal number: ");
+// console.log(Math.round(num));
+
 
 //STEP 3
+// var num = window.prompt("Enter a decimal number: ");
+// console.log(Math.floor(num));
 
 //STEP 4
+// var num = parseInt(window.prompt("Enter any 5 numbers separated by comma(,): "));
+// console.log("Largest number is: " + Math.max(num) + "\nSmalest number is: " + Math.min(num));
+
 
 //STEP 5
+// var num = window.prompt("Enter a number: ");
+// console.log(Math.sqrt(num));
