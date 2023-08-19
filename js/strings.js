a43a0d1dcf3d363de2508f9b02bf5d7ab59c55f9
Monali Valunj <monavalunj96@gmail.com>
2023-08-19 00:08:39 -0700

    working on string operations

--- js/strings.js
+++ js/strings.js
@@ -1,8 +1,18 @@
 //STEP 1
+// var myName = window.prompt("Enter your name: ");
+// window.alert("your  name is "+ myName.length + " charater long")
 
 //STEP 2
+// var myName = window.prompt("Enter your name: ");
+// var n = window.prompt("Enter number")
+// window.alert("Charater at:  "+ myName.charAt(n))
+
 
 //STEP 3
+var fName = window.prompt("Enter your first name: ");
+var lName = window.prompt("Enter your last name: ");
+
+window.alert("Your name is: " + fName.concat(lName))
 
 //STEP 4
 
