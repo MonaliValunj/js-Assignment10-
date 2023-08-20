5ccdb795e19c36fef0c5d31df8fe8d9a50c4a867
Monali Valunj <monavalunj96@gmail.com>
2023-08-19 23:59:16 -0700

    date js in progress

--- js/dates.js
+++ js/dates.js
@@ -1,6 +1,10 @@
 //STEP 1
+// let d = new Date()
+// console.log("Number of days in a month : " + d.getMonth())
 
 //STEP 2
+let d = new Date()
+console.log("month is : " + d.getMonth())
 
 //STEP 3
 
--- js/strings.js
+++ js/strings.js
@@ -9,21 +9,43 @@
 
 
 //STEP 3
-var fName = window.prompt("Enter your first name: ");
-var lName = window.prompt("Enter your last name: ");
+// var fName = window.prompt("Enter your first name: ");
+// var lName = window.prompt("Enter your last name: ");
 
-window.alert("Your name is: " + fName.concat(lName))
+// window.alert("Your name is: " + fName.concat(" ", lName))
 
 //STEP 4
+// var text = "The quick brown fox jumps over the lazy dog";
+// window.alert("index of 'fox' is : "+ text.indexOf("fox"));
 
 //STEP 5
+// var text = "The quick brown fox jumps over the lazy fox";
+// window.alert("last index of 'fox' is : "+ text.lastIndexOf("fox"));
 
 //STEP 6
+// var text = "The quick brown fox jumped over the lazy dog";
+// var userName = window.prompt("Enter your name");
+// var newText = text.replace("the lazy dog", userName)
+// window.alert("new text: "+ newText);
 
 //STEP 7
+// var text = "The quick brown fox jumped over the lazy dog";
+// var word = window.prompt("Enter word you looking for: ");
+// var result = text.search(word)
+// window.alert(word + " word found at index "+ result);
+
 
 //STEP 8
+// var old_string = "The quick brown fox jumps over the lazy dog";
+// var new_string = old_string.slice(31,43)
+// window.alert(new_string.toUpperCase())
 
 //STEP 9
+// var text = "            THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG           ";
+// window.alert(text.trim().toLocaleLowerCase())
 
 //STEP 10
+// var text = "the quick brown fox jumps over the lazy dog";
+// var firstLetter = text.charAt(0).toUpperCase();
+// var sentence = text.slice(1)
+// window.alert(firstLetter+sentence);
