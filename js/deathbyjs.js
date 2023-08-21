de8248d386577b677ca65223c92bb28ef90db6db
Monali Valunj <monavalunj96@gmail.com>
2023-08-20 18:20:06 -0700

    -

--- js/deathbyjs.js
+++ js/deathbyjs.js
@@ -1,6 +1,31 @@
 //STEP 1
+// function sortAlphabeticaly(str){
+//     return str.split("").sort().join("")
+    
+// } 
+// console.log(sortAlphabeticaly('webmaster'))
 
 //STEP 2
+//function ConverToUppercase(str){
+    let str = 'the quick brown fox'
+    //contver string to array of words
+    var words = str.split(" ")
+    console.log(words)
+    for(var i = 0; i< words.length; i++){
+        words[i] = words[i].charAt(0).toUpperCase()
+        var remaning = words[i].slice(1)
+        var capWords = firstLetter+remaning
+       // console.log( capWords)
+
+    }
+    var result = words.join(" ");
+    console.log(result)
+
+    //return str.charAt(0).toUpperCase()+str.substring(1)
+
+//}
+
+//console.log(ConverToUppercase('the quick brown fox'))
 
 //STEP 3
 
