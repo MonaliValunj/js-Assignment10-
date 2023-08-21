71412c15c38be416302a1f651aeaf03ba66b7f15
Monali Valunj <monavalunj96@gmail.com>
2023-08-20 17:33:14 -0700

    date js completed

--- js/dates.js
+++ js/dates.js
@@ -3,11 +3,32 @@
 // console.log("Number of days in a month : " + d.getMonth())
 
 //STEP 2
-let d = new Date()
-console.log("month is : " + d.getMonth())
+
+// var d = new Date('2023-03-24');
+// var month = d.toLocaleString('default', { month: 'long' });
+// console.log(month);
 
 //STEP 3
+// var today = new Date();
+// var day = today.getDay();
+// console.log(today + day);
+
+// if(day === 0 || day === 6){
+//     console.log("its weekend")
+// }else{
+//     console.log("Its not weekend yet    ")
+// }
 
 //STEP 4
+// var today = new Date();
+// var yesterday = today.getDate() - 1;
+// console.log(today);
+// console.log("Yesterday  was - "+ yesterday);
 
 //STEP 5
+// var weekdays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+// var d = new Date();
+// var day = weekdays[d.getDay()]
+// let firstLetterOfDay = day.charAt(0);
+
+// console.log("Today is: " + firstLetterOfDay)
